5b3857a0e9e77e05c9471fc28ba4dc605cb39433
matheinz <matthew_heinz_sanchez@dlsu.edu.ph>
2026-07-27 17:11:11 +0800

    feat: added container class to hold randomized items where player can choose from

---
+++ src/main/java/com/matcheww/workshop/model/ItemContainer.java
@@ -0,0 +1,9 @@
+package com.matcheww.workshop.model;
+
+public class ItemContainer extends Container {
+    public static final int SIZE = 54;
+
+    public ItemContainer(){
+        super(SIZE);
+    }
+}
